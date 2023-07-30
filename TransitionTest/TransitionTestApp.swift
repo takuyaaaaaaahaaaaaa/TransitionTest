c1b756e5944b13a83d1951c9fe749226a1379653
takuyaaaaaaahaaaaaa <majikani2011@gmail.com>
2023-07-30 10:10:49 +0900

    Initial Commit

---
+++ TransitionTest/TransitionTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransitionTestApp.swift
+//  TransitionTest
+//
+//  Created by takuya_tominaga on 7/30/23.
+//
+
+import SwiftUI
+
+@main
+struct TransitionTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
